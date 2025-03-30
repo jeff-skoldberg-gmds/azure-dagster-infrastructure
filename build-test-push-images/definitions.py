3d6a64ddb930458f10e01e4c18463add0cab55a9
Jeff Skoldberg <jeff@gmdata.co>
2025-03-29 22:55:40 -0400

    works

--- build-test-push-images/definitions.py
+++ build-test-push-images/definitions.py
@@ -1,14 +1,18 @@
 import dagster as dg
 
 
-@dg.asset(
-    op_tags={"operation": "example"},
-    partitions_def=dg.DailyPartitionsDefinition("2024-01-01"),
+@dg.asset()
+def example_asset1():
+    print("This is example_asset1")
+
+@dg.asset(deps=[example_asset1])  
+def example_asset2():
+    print("This is example_asset2")
+
+partitioned_asset_job = dg.define_asset_job(
+    name="partitioned_asset_job",
+    selection=[example_asset1, example_asset2],  # Select the assets you want to include in the job
+    # partitions_def=dg.HourlyPartitionsDefinition(start_date="2023-01-01-00:00")  # Example partition definition
 )
-def example_asset(context: dg.AssetExecutionContext):
-    context.log.info(context.partition_key)
 
-
-partitioned_asset_job = dg.define_asset_job("partitioned_job", selection=[example_asset])
-
-defs = dg.Definitions(assets=[example_asset], jobs=[partitioned_asset_job])
+defs = dg.Definitions(assets=[example_asset1,example_asset2], jobs=[partitioned_asset_job])
